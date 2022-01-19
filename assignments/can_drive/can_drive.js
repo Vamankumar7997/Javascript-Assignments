477396a15b3a02a91056ff29fcacba58e12e61aa
VAMAN KUMAR TAMARANA <tamaranavamankumar@gmail.com>
2022-01-19 15:33:42 +0530

    Final submission for candrive commit

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,29 +1,12 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
-    // if (hasDrivingLiscence == false && isTired == false && isSober == true) {
-    //     return "You cannot drive"
-    // }
-    // else if (hasDrivingLiscence == false && isTired == true && isSober == true) {
-    //     return "You cannot drive"
-    // }
-    // else if (hasDrivingLiscence == true && isTired == false && isSober == false) {
-    //     return "You shouldn't drive"
-    // }
-    // else if (hasDrivingLiscence == true && isTired == true && isSober == false) {
-    //     return "You shouldn't drive"
-    // }
-
-    // else if (hasDrivingLiscence == true && isTired == false && isSober == true) {
-    //     return "You can drive"
-    // }
+    
     if (hasDrivingLiscence == false){
         return "You cannot drive"
     }
     if (isTired == true || isSober == false) {
         return "You shouldn't drive"
     }
-    
     return "You can drive"
-
 }
     // Write you code here
 
