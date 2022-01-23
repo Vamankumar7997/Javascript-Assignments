85a4282811cb78467b0f3eb77553524ec7d87342
VAMAN KUMAR TAMARANA <tamaranavamankumar@gmail.com>
2022-01-23 16:13:56 +0530

    Cfreate object submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,4 +1,13 @@
 function CreateObject(arr) {
+    let values = {}
+    let res = 0
+    while (res < arr.length) {
+        let ans = arr[res]
+        let obj = arr[res + 1]
+        values[ans] = obj
+        res = res + 2
+    }
+    return values
     // Write your code here
 }
 
