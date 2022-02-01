cbfdb8d4e0063d2eb59e6ba0ce237622d0287d04
VAMAN KUMAR TAMARANA <tamaranavamankumar@gmail.com>
2022-02-02 00:02:35 +0530

    Todo list API changes

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,24 +1,5 @@
 
-// var input = document.querySelector("input");
 
-// var addBtn = document.querySelector("button");
-// var todoList = document.querySelector('.todos');
-// var count = 0;
-
-// addBtn.addEventListener('click', function () {
-//     var data = input.value;
-//     var para = document.createElement("p");
-//     para.setAttribute("key", count);
-//     para.append(data);
-//     todoList.append(para);
-//     input.value = "";
-//     count += 1
-
-//     para.addEventListener('click', function () {
-//         para.remove()
-//     })
-
-// })
 
 
 var button = document.querySelector("button")
